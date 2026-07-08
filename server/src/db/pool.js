ebc0b492b908261681714f13220020a869567cb8
Elias Nichols <eliastheviking@gmail.com>
2026-07-08 10:46:44 -0700

    fixed auth

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -5,6 +5,9 @@ const { Pool } = pg
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('rds.amazonaws.com')
+    ? { rejectUnauthorized: false }
+    : false,
 })
 
 pool.on('error', (err) => {
